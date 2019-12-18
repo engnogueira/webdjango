af098fec2cae141fa1c1b276a778f27822ed9e50
engnogueira <engnogueira@hotmail.com>
2019-12-18 14:43:23 -0300

    Configuração da linguagem e do fuso horário

--- pypro/settings.py
+++ pypro/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = config('LANGUAGE_CODE', default=pt-br')
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config('TIME_ZONE', default='America/Sao Paulo')
 
 USE_I18N = True
 
