e083aaefdc17370bd825ad20df492e73767ec0f0
engnogueira <engnogueira@hotmail.com>
2019-12-25 08:26:25 -0300

    Publicando o Video em Produção - Aula 2.2.7 close #65

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
@@ -9,3 +10,11 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/381369324"')
