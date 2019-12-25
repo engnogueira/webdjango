0cc776968723d679378c689667bc3d36ebf3440e
engnogueira <engnogueira@hotmail.com>
2019-12-25 15:43:04 -0300

    Video de Acordo com o Slug - Aula 2.3.5 close #70

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 381369324}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 381369324},
+        'instalacao-windows': {'titulo': 'Video Aperitivo: Instalação Windows', 'vimeo_id': 381437493}
+    }
+    video = videos[slug]
     return render(request, "aperitivos/video.html", context={'video': video})
