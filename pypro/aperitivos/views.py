8f4e4c0b2425a8dd35534f281f261ded4e7e186d
engnogueira <engnogueira@hotmail.com>
2020-01-07 19:57:36 -0300

    Modelos do Django - Aula 3.1.4 close #79

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Motivação', 381369324),
-    Video('instalacao-windows', 'Instalação Windows', 381437493),
+    Video(slug='motivacao', titulo='Motivação', vimeo_id='381369324'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='381437493'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
