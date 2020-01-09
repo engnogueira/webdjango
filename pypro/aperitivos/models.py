e12488d4c037a64def4e0631efa6099961107ed2
engnogueira <engnogueira@hotmail.com>
2020-01-09 02:23:41 -0300

    Introdução a QuerySet e Migrações - Aula 3.1.5 close #80

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.db.models import Model
 from django.urls import reverse
 
 
-class Video(models.Model):
+class Video(Model):
     slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
-
+    
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
